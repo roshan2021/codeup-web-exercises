75b254e4a766fa8656c0c70f5877c18cf5816195
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-03-18 15:40:04 -0500

    created 3 branch

--- js/external.js
+++ js/external.js
@@ -1,6 +1,7 @@
 "use strict";
 
 console.log("Hello from external JavaScript.");
+
 alert("Welcome to my Website!");
 
 var favColor = prompt('What is your favorite color?');
---
+++ js/functions.js
@@ -0,0 +1,153 @@
+"use strict";
+
+// console.log("hello");
+
+/**
+ * TODO:
+ * Create a function called 'sayHello' that takes a parameter 'name'.
+ * When called, the function should return a message that says hello to the passed in name.
+ *
+ * Example
+ * > sayHello("codeup") // returns "Hello, codeup!"
+ * */
+
+function sayHello(name) {
+    return "Hello, " + name + "!";
+}
+
+console.log(sayHello("Codeup"));
+console.log(sayHello("Roshan"));
+console.log(sayHello("Apex"));
+
+
+/**
+ * TODO:
+ * Call the function 'sayHello' and pass your name as a string literal argument.
+ * Store the result of the function call in a variable named 'helloMessage'.
+ *
+ * console.log 'helloMessage' to check your work
+ */
+
+var helloMessage =  sayHello("Part 2 Roshan");
+console.log(helloMessage);
+
+helloMessage = sayHello("Part 2 Codeup");
+console.log(helloMessage);
+
+helloMessage = sayHello("Part 2 Apex");
+
+
+/**
+ * TODO:
+ * Store your name as a string in a variable named 'myName', and pass that
+ * variable to the 'sayHello' function. You should see the same output in the
+ * console.
+ */
+
+var myName = "Roshan"
+console.log("Part 3: var myName = ", sayHello(myName));
+
+
+// Don't modify the following line, it generates a random number between 1 and 3
+// and stores it in a variable named random
+var random = Math.floor((Math.random() * 3) + 1);
+
+/**
+ * TODO:
+ * Create a function called 'isTwo' that takes a number as a parameter.
+ * The function should return a boolean value based on whether or not the passed
+ * number is the number 2.
+ *
+ * Example
+ * > isTwo(1) // returns false
+ * > isTwo(2) // returns true
+ * > isTwo(3) // returns false
+ *
+ * Call the function 'isTwo' passing the variable 'random' as a argument.
+ *
+ * console.log *outside of the function* to check your work (you should see a
+ * different result everytime you refresh the page if you are using the random
+ * number)
+ */
+
+function isTwo(aNumber) {
+    return 2 === aNumber;
+}
+
+// console.log(isTwo(1));
+// console.log(isTwo(2));
+// console.log(isTwo(3));
+
+
+console.log("random = " + random, isTwo(random));
+
+
+/**
+ * TODO:
+ * Create a function named 'calculateTip' to calculate a tip on a bill at a
+ * restaurant. The function should accept a tip percentage and the total of the
+ * bill, and return the amount to tip
+ *
+ * Examples:
+ * > calculateTip(0.20, 20) // returns 4
+ * > calculateTip(0.25, 25.50) // returns 6.37
+ * > calculateTip(0.15, 33.42) // returns 5.013
+ */
+
+
+function calculateTip(tip, total) {
+    return tip * total;
+}
+
+console.log("calculateTip (0.20, 20)", calculateTip(0.20, 20));
+// console.log("calculateTip (0.25, 25.50)", calculateTip(0.25, 25.50);
+// console.log("calculateTip (0.30, 100)", calculateTip(0.30, 100);
+
+
+
+
+
+/**
+ * TODO:
+ * Use prompt and alert in combination with your calculateTip function to
+ * prompt the user for the bill total and a percentage they would like to tip,
+ * then display the dollar amount they should tip
+ */
+
+var bill = prompt("Give me the total");
+var percentage = prompt ("Give me the percentage");
+
+//Make user's life easier
+percentage = percentage / 100;
+
+// var tipTotal = calculateTip( (percentage / 100), bill);
+var tipTotal = calculateTip(percentage, bill);
+alert(tipTotal);
+
+
+
+/**
+ * TODO:
+ * Create a function named `applyDiscount`. This function should accept a price
+ * (before a discount is applied), and a discount percentage (a number between 0
+ * and 1). It should return the result of applying the discount to the original
+ * price.
+ *
+ * Example:
+ * > var originalPrice = 100;
+ * > var dicountPercent = .2; // 20%
+ * > applyDiscount(originalPrice, dicountPercent) // 80
+ *
+ * > applyDiscount(45.99, 0.12) // 40.4712
+ */
+
+function applyDiscount(price, discount) {
+    return price - (price * discount);
+}
+
+console.log(applyDiscount(100, 0.2));
+console.log(applyDiscount(45.99, 0.12));
+
+
+
+
