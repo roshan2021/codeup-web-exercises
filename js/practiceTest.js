6b129faa9a7d4773bb5899782309fb6cb4521ef5
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-04-07 08:40:47 -0500

    practice round

--- js/practiceTest.js
+++ js/practiceTest.js
@@ -1,8 +1,17 @@
 
 "use strict";
 
-if(99 !== "99") {
-    console.log("A number equals a string!");
-}   else {
-    console.log("No way a number equals a string");
-}
+// if(99 !== "99") {
+//     console.log("A number equals a string!");
+// }   else {
+//     console.log("No way a number equals a string");
+// }
+
+
+var array = ['John', 'Paul', 'George', 'Ringo', 'Santana'];
+
+console.log(array[array.length - 2]);
+
+
+
+
